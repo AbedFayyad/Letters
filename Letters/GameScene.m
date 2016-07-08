401d3acecceb94bafbc688334ef574efecabe6c4
Abed Fayyad <AbedFayyad@live.com>
2016-07-08 13:49:00 +0300

    Clean up game scene grid creation

--- Letters/GameScene.m
+++ Letters/GameScene.m
@@ -22,32 +22,11 @@
     
     self.backgroundColor = [SKColor whiteColor];
     
-    self.grid = [[LetterGrid alloc] initWithSize:CGSizeMake(414.0f, 552.0f)];
+    self.grid = [[LetterGrid alloc] initWithSize:CGSizeMake(self.size.width * 0.85,
+                                                            self.size.width * 0.85 * (4.0 / 3.0))];
     self.grid.gridNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     
     [self addChild:self.grid.gridNode];
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        [self.grid setActivePosition:[touch locationInNode:self.grid.gridNode]];
-    }
-}
-
-- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch ends */
-    
-    [self.grid resetActivePosition];
-}
-
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch moves */
-    
-    for (UITouch *touch in touches) {
-        [self.grid slideTo:[touch locationInNode:self.grid.gridNode]];
-    }
 }
 
 - (void)update:(CFTimeInterval)currentTime {
